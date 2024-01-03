5a00470c668942b2c31332f0d54a104af7d2101b
lolOzzi <47964440+lolOzzi@users.noreply.github.com>
2024-01-03 11:50:16 +0100

    Update DesktopLauncher.java

--- desktop/src/com/snake/game/DesktopLauncher.java
+++ desktop/src/com/snake/game/DesktopLauncher.java
@@ -1,16 +1,20 @@
 package com.snake.game;
 
+import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.glutils.HdpiMode;
 import com.snake.game.SnakeProjekt;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setWindowedMode(1920, 1080);
-		config.setForegroundFPS(60);
-		config.setTitle("IntroSnakeProjekt");
-		new Lwjgl3Application(new SnakeProjekt(), config);
-	}
+    public static void main (String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        DisplayMode desktopMode = Lwjgl3ApplicationConfiguration.getDisplayMode();
+        config.setFullscreenMode(desktopMode);
+        config.setHdpiMode(HdpiMode.Pixels);
+        config.setForegroundFPS(60);
+        config.setTitle("IntroSnakeProjekt");
+        new Lwjgl3Application(new SnakeProjekt(), config);
+    }
 }
