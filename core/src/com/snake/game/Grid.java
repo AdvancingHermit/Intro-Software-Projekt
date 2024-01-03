b68086c83db694d5e981a645463fb667e28112eb
lolOzzi <47964440+lolOzzi@users.noreply.github.com>
2024-01-03 13:43:36 +0100

    Update Grid.java

--- core/src/com/snake/game/Grid.java
+++ core/src/com/snake/game/Grid.java
@@ -9,12 +9,12 @@ import java.util.ArrayList;
 
 public class Grid {
     Snake[] snakes;
-    int snakeSize;
+    int squareSize;
     int gridSize;
 
     Grid(int gridSize, int snakeAmount) {
         snakes = new Snake[snakeAmount];
-        snakeSize = 30;
+        squareSize = 30;
         this.gridSize = gridSize;
         for (int i = 0; i < snakeAmount; i++) {
             snakes[i] = new Snake(gridSize, gridSize);
@@ -29,14 +29,14 @@ public class Grid {
                 snakes[i].move();
             }
         }
-        double xOffset = (game_width / 2) - (snakeSize*gridSize*0.6);
-        double yOffset = (game_height / 2) - (snakeSize*gridSize* 0.6 );
+        double xOffset = (game_width / 2) - (squareSize*gridSize*0.6);
+        double yOffset = (game_height / 2) - (squareSize*gridSize* 0.6 );
   
         Rectangle[][] rects = new Rectangle[gridSize][gridSize];
         for (int i = 0; i < gridSize; i++) {
             for (int j = 0; j < gridSize; j++) {
-                rects[i][j] = new Rectangle((int) (i * snakeSize * 1.2 + xOffset),
-                        (int) (j * snakeSize * 1.2 + yOffset), snakeSize, snakeSize);
+                rects[i][j] = new Rectangle((int) (i * squareSize * 1.2 + xOffset),
+                        (int) (j * squareSize * 1.2 + yOffset), squareSize, squareSize);
             }
         }
         return rects;
