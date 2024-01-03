46804aee28c9fa06a02c8ac96a8d5f6751fe1bcc
lolOzzi <47964440+lolOzzi@users.noreply.github.com>
2024-01-03 09:13:58 +0100

    fixed aspect ratio

--- desktop/src/com/snake/game/DesktopLauncher.java
+++ desktop/src/com/snake/game/DesktopLauncher.java
@@ -8,6 +8,7 @@ import com.snake.game.SnakeProjekt;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(1280, 720);
 		config.setForegroundFPS(60);
 		config.setTitle("IntroSnakeProjekt");
 		new Lwjgl3Application(new SnakeProjekt(), config);
